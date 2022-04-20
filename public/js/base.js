41b2ca8df436a092df1f5e35b97d0b821963cdbf
Jonatas Alefe <alefepup@gmail.com>
2022-04-20 18:03:05 -0300

    Iciando o GIT

---
+++ public/js/base.js
@@ -0,0 +1,20 @@
+const nome = "Marcelo Eltz";
+let nome2 = "Marcelo Eltz"
+
+nome = "Pedro Silva"
+
+console.log("valor inicial:")
+console.log(nome2)
+
+console.log("valor alterado:")
+console.log(nome2)
+
+function alterarNome() {
+    nome2 "Maria Silva"
+}
+
+alterarNome();
+
+
+
+
